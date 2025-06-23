fb71e5aaf29332b74935838842338955a65ee3a2
OlegMenjok <olegmenjok@gmail.com>
2025-06-23 20:14:19 +0300

    fix comments

--- homework8.1/task2.js
+++ homework8.1/task2.js
@@ -1,5 +1,5 @@
 const array = [1, 2, 3, 4, 5];
-const doubledNumbers = array.map(item => item * 3);
+const doubledNumbers = array.map((item, index) => item * index);
 
 console.log(array);
 console.log(doubledNumbers)
--- homework8.1/task4.js
+++ homework8.1/task4.js
@@ -1,10 +1,6 @@
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const evenArray = [];
+let evenArray = [];
 
-for(let i = 0; i < array.length; i++){
-    if(array[i] % 2 === 0){
-        evenArray.push(array[i])
-    }
-}
+evenArray = array.filter(item => item % 2 === 0)
 
 console.log(evenArray)
