fa3fe8850eb57cc3bc51b8ecdcc3123e1d36f931
OlegMenjok <olegmenjok@gmail.com>
2025-07-03 21:34:37 +0300

    check

--- homework6.1/task1.js
+++ homework6.1/task1.js
@@ -1,7 +1,7 @@
 function getAreaOfRectangle1(width, height) {
   return width * height;
 }
-console.log(`Area find by function declaration: ${getAreaOfRectangle1(12, 4)}`)
+console.log(`Area find by function declaration: ${getAreaOfRectangle1(12, 4)}`);
 
 const getAreaOfRectangle2 = function (width, height) {
   return width * height;
