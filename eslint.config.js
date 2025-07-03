aa8827c51cdaba2e2094c36667a50dcb889133fa
OlegMenjok <olegmenjok@gmail.com>
2025-07-03 21:02:20 +0300

    Resolve HW12.1

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+
+export default [
+  {
+    files: ['**/*.js'],
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      indent: ['error', 2],
+    },
+  },
+];
+
