1ec6b670c2099bf845a1755e43cbb97a96a08c23
OlegMenjok <olegmenjok@gmail.com>
2025-07-20 11:46:36 +0300

    fix

--- homework5.1/task2.js
+++ homework5.1/task2.js
@@ -2,18 +2,18 @@ const averageGrade = 91;
 
 switch (true) {
 case averageGrade < 60:
-  console.log('Незадовільно');
-  break;
-case averageGrade <= 70:
-  console.log('Задовільно');
-  break;
-case averageGrade <= 80:
-  console.log('Добре');
-  break;
-case averageGrade <= 90:
-  console.log('Дуже добре');
-  break;
-case averageGrade <= 100:
-  console.log('Відмінно');
-  break;
+    console.log('Незадовільно');
+    break;
+  case averageGrade <= 70:
+    console.log('Задовільно');
+    break;
+  case averageGrade <= 80:
+    console.log('Добре');
+    break;
+  case averageGrade <= 90:
+    console.log('Дуже добре');
+    break;
+  case averageGrade <= 100:
+    console.log('Відмінно');
+    break;
 }
